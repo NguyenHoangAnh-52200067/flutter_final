930033d4e9bc9247de67d17c02628ba99a34f150
Lâm Nhật Hào <52200029@student.tdtu.edu.vn>
2025-04-27 23:17:43 +0700

    header + home category

---
+++ lib/screens/widgets/form/home_category.dart
@@ -0,0 +1,46 @@
+import 'package:ecomerce_app/models/category_model.dart';
+import 'package:ecomerce_app/screens/widgets/grid/category_products_grid.dart';
+import 'package:ecomerce_app/screens/widgets/image_text_widget/vertical_image_text.dart';
+import 'package:ecomerce_app/utils/image_utils.dart';
+import 'package:flutter/material.dart';
+
+class HomeCategories extends StatelessWidget {
+  final List<CategoryModel> categories;
+
+  const HomeCategories({super.key, required this.categories});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      width: double.infinity,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: categories.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) {
+          final category = categories[index];
+          return VerticalImageText(
+            image: Image.network(category.imageUrl!, width: 30, height: 30),
+            title: category.name,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder:
+                      (context) => CategoryProductsGrid(
+                        categoryId: category.id!,
+                        crossAxisCount: 2,
+                        padding: const EdgeInsets.all(8),
+                        scrollController: ScrollController(),
+                        categoryName: category.name,
+                      ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
