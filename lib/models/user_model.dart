b6bfe39fbecbd544a9a29eda5dfd41b581fc32f3
Lâm Nhật Hào <52200029@student.tdtu.edu.vn>
2025-04-30 15:27:19 +0700

    models

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,15 +2,23 @@ class UserModel {
   String? id;
   String email;
   String fullName;
-  String address;
+  String? address;
   String? linkImage;
+  bool isBanned = false;
 
+  int? memberShipPoint;
+  int? memberShipCurrentPoint;
+  String? memberShipLevel;
   UserModel({
     this.id,
     required this.email,
     required this.fullName,
-    required this.address,
+    this.address,
     this.linkImage,
+    this.memberShipPoint = 0,
+    this.memberShipCurrentPoint = 0,
+    this.memberShipLevel = 'Thành viên',
+    this.isBanned = false,
   });
 
   Map<String, dynamic> toJson() {
@@ -20,6 +28,10 @@ class UserModel {
       "fullName": fullName,
       "address": address,
       "imageLink": linkImage,
+      "memberShipPoints": memberShipPoint,
+      "memberShipLevel": memberShipLevel,
+      "memberShipCurrentPoint": memberShipCurrentPoint,
+      "isBanned": isBanned,
     };
   }
 
@@ -30,6 +42,10 @@ class UserModel {
       fullName: json["fullName"],
       address: json["address"],
       linkImage: json["imageLink"],
+      memberShipPoint: json["memberShipPoints"],
+      memberShipLevel: json["memberShipLevel"],
+      memberShipCurrentPoint: json["memberShipCurrentPoint"],
+      isBanned: json["isBanned"] ?? false,
     );
   }
 }
