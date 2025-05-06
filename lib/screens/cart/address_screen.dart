2921c8575d360639de98442c074b78ea248b261d
meouisme <118751514+meouisme@users.noreply.github.com>
2025-05-06 19:33:10 +0700

    update cart

---
+++ lib/screens/cart/address_screen.dart
@@ -0,0 +1,303 @@
+import 'package:ecomerce_app/models/address_model.dart';
+import 'package:ecomerce_app/models/user_model.dart';
+import 'package:ecomerce_app/repository/address_repository.dart';
+import 'package:ecomerce_app/repository/user_repository.dart';
+import 'package:ecomerce_app/screens/cart/edit_address_screen.dart';
+import 'package:ecomerce_app/screens/cart/new_address_screen.dart';
+import 'package:flutter/material.dart';
+
+class AddressScreen extends StatefulWidget {
+  @override
+  _AddressScreenState createState() => _AddressScreenState();
+}
+
+class _AddressScreenState extends State<AddressScreen> {
+  final UserRepository _userRepository = UserRepository();
+  final AddressRepository _addressRepository = AddressRepository();
+  UserModel? userModel;
+
+  int selectedAddressIndex = 0;
+  List<AddressModel> addressList = [];
+  bool _isUpdatingAddress = false;
+
+  void _setDefaultAddress(int index) async {
+    // Kiểm tra nếu đang trong quá trình cập nhật thì không thực hiện
+    if (_isUpdatingAddress) return;
+
+    // Đánh dấu đang trong quá trình cập nhật
+    setState(() {
+      _isUpdatingAddress = true;
+    });
+
+    try {
+      String selectedId = addressList[index].addressId;
+
+      // Tìm địa chỉ mặc định hiện tại và cập nhật thành false
+      for (var addr in addressList) {
+        if (addr.isDefault) {
+          await _addressRepository.updateDefaultAddress(addr.addressId, false);
+        }
+      }
+
+      // Cập nhật địa chỉ được chọn thành mặc định
+      await _addressRepository.updateDefaultAddress(selectedId, true);
+
+      // Cập nhật UI
+      setState(() {
+        for (int i = 0; i < addressList.length; i++) {
+          // Tạo bản sao của địa chỉ để tránh lỗi với final fields
+          addressList[i] = AddressModel(
+            addressId: addressList[i].addressId,
+            userId: addressList[i].userId,
+            city: addressList[i].city,
+            district: addressList[i].district,
+            ward: addressList[i].ward,
+            street: addressList[i].street,
+            local: addressList[i].local,
+            fullAddress: addressList[i].fullAddress,
+            userName: addressList[i].userName,
+            userPhone: addressList[i].userPhone,
+            isDefault: (i == index),
+            userMail: addressList[i].userMail,
+          );
+        }
+        selectedAddressIndex = index;
+      });
+    } catch (e) {
+      print("Lỗi khi cập nhật địa chỉ mặc định: $e");
+      // Hiển thị thông báo lỗi cho người dùng
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Không thể cập nhật địa chỉ: $e")));
+    } finally {
+      // Đánh dấu đã hoàn thành quá trình cập nhật
+      setState(() {
+        _isUpdatingAddress = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUser();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    fetchUser();
+  }
+
+  Future<void> fetchUser() async {
+    try {
+      String userId = await _userRepository.getEffectiveUserId();
+      final addresses = await _addressRepository.getAddressesByUserId(userId);
+
+      setState(() {
+        addressList = addresses;
+        int defaultIndex = addresses.indexWhere((addr) => addr.isDefault);
+        selectedAddressIndex = defaultIndex >= 0 ? defaultIndex : 0;
+      });
+    } catch (e) {
+      print("Lỗi khi tải thông tin người dùng: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Không thể tải thông tin địa chỉ")),
+      );
+    }
+  }
+
+  void _editAddress(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditAddressScreen(address: addressList[index]),
+      ),
+    ).then((_) => fetchUser());
+  }
+
+  void _addNewAddress() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NewAddressScreen()),
+    );
+
+    if (result == true) {
+      await fetchUser();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chọn địa chỉ nhận hàng"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context, true),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child:
+                addressList.isEmpty
+                    ? Center(
+                      child: Text(
+                        "Chưa có địa chỉ nào. Vui lòng thêm địa chỉ mới.",
+                      ),
+                    )
+                    : ListView.builder(
+                      itemCount: addressList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
+                          ),
+                          child: InkWell(
+                            onTap:
+                                _isUpdatingAddress
+                                    ? null
+                                    : () => _setDefaultAddress(index),
+                            child: ListTile(
+                              leading: Radio<int>(
+                                value: index,
+                                groupValue: selectedAddressIndex,
+                                onChanged:
+                                    _isUpdatingAddress
+                                        ? null
+                                        : (int? value) {
+                                          if (value != null) {
+                                            _setDefaultAddress(value);
+                                          }
+                                        },
+                              ),
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      addressList[index].userName,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                      softWrap: true,
+                                    ),
+                                  ),
+                                  // if (addressList.length > 1 &&
+                                  //     selectedAddressIndex != index)
+                                  TextButton(
+                                    onPressed: () {
+                                      _editAddress(index);
+                                    },
+                                    child: Text(
+                                      "Sửa",
+                                      style: TextStyle(color: Colors.blue),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    addressList[index].fullAddress,
+                                    style: TextStyle(
+                                      color: const Color.fromARGB(
+                                        255,
+                                        59,
+                                        59,
+                                        59,
+                                      ),
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                    softWrap: true,
+                                  ),
+                                  if (addressList[index].isDefault == true)
+                                    Container(
+                                      margin: EdgeInsets.only(top: 6),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                        vertical: 4,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Colors.red.shade100,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Text(
+                                        "Mặc định",
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton.icon(
+              onPressed: _addNewAddress,
+              icon: Icon(Icons.add, color: Colors.white),
+              label: Text("Thêm Địa Chỉ Mới"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                minimumSize: Size(double.infinity, 45),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// void _showConfrim() {
+//   showDialog(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return AlertDialog(
+//         title: Text('Xác nhận'),
+//         content: Text('Bạn có muốn xóa địa chỉ này hay không?'),
+//         actions: [
+//           TextButton(
+//             onPressed: () => Navigator.pop(context),
+//             child: Text('Không', style: TextStyle(fontSize: 18)),
+//           ),
+//           TextButton(
+//             onPressed: () async {
+//               Navigator.pop(context);
+//               _deleteAddress(index);
+//               await fetchUser();
+//             },
+//             child: Text(
+//               'Đồng ý',
+//               style: TextStyle(color: Colors.red, fontSize: 18),
+//             ),
+//           ),
+//         ],
+//       );
+//     },
+//   );
+// }
---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,335 @@
+import 'package:ecomerce_app/home.dart';
+import 'package:ecomerce_app/models/cart_model.dart';
+import 'package:ecomerce_app/models/cartitems_model.dart';
+import 'package:ecomerce_app/repository/cart_repository.dart';
+import 'package:ecomerce_app/repository/product_repository.dart';
+import 'package:ecomerce_app/screens/cart/checkout_screen.dart';
+import 'package:ecomerce_app/utils/utils.dart';
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatefulWidget {
+  final Cart cart;
+  const CartScreen({super.key, required this.cart});
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  final CartRepository _cartRepository = CartRepository();
+  final ProductRepository _productRepository = ProductRepository();
+
+  late List<CartItem> _cartItems;
+  final Set<String> _selectedItems = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _cartItems = widget.cart.items;
+  }
+
+  double _calculateSelectedTotal() => _cartItems
+      .where((item) => _selectedItems.contains(item.id))
+      .fold(
+        0,
+        (total, item) =>
+            total +
+            ((item.discountRate != null && item.discountRate! > 0
+                    ? item.priceAfterDiscount!
+                    : item.price) *
+                item.quantity),
+      );
+
+  Future<void> _removeItem(CartItem item) async {
+    await _productRepository.undoProductStock(item.productId, item.quantity);
+    await _cartRepository.removeItem(widget.cart, item.id);
+    setState(() {
+      _cartItems.remove(item);
+      _selectedItems.remove(item.id);
+    });
+  }
+
+  Future<void> _updateItemQuantity(CartItem item, int newQuantity) async {
+    if (newQuantity < 1) {
+      _showDeleteConfirmationDialog(item);
+      return;
+    }
+
+    await _cartRepository.updateItemQuantity(widget.cart, item.id, newQuantity);
+    setState(() => item.quantity = newQuantity);
+  }
+
+  void _showDeleteConfirmationDialog(CartItem item) {
+    showDialog(
+      context: context,
+      builder:
+          (BuildContext context) => AlertDialog(
+            title: const Text('Xác nhận'),
+            content: const Text('Bạn có muốn xóa sản phẩm này khỏi giỏ hàng?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Không', style: TextStyle(fontSize: 18)),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _removeItem(item);
+                },
+                child: const Text(
+                  'Đồng ý',
+                  style: TextStyle(color: Colors.red, fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+
+  void _navigateToCheckout() {
+    final selectedProducts =
+        _cartItems.where((item) => _selectedItems.contains(item.id)).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CheckoutScreen(cartItems: selectedProducts),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 238, 238, 238),
+      appBar: AppBar(title: const Text('Giỏ hàng')),
+      body:
+          _cartItems.isEmpty
+              ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16), // Bo tròn 4 góc
+                      child: Image.network(
+                        'https://cdni.iconscout.com/illustration/premium/thumb/online-shopping-cart-2748733-2289776.png',
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+
+                    const Text(
+                      '"Hổng" có gì trong giỏ hết',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Lướt ShopOnline, lựa hàng ngay đi!',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomeScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text(
+                        'Mua sắm ngay!',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+              : SingleChildScrollView(
+                child: Column(
+                  children: _cartItems.map(_buildCartItem).toList(),
+                ),
+              ),
+      bottomNavigationBar: _cartItems.isEmpty ? null : _buildBottomBar(),
+    );
+  }
+
+  Widget _buildBottomBar() {
+    return BottomAppBar(
+      color: const Color.fromARGB(255, 233, 233, 233),
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Tổng cộng: ${Utils.formatCurrency(_calculateSelectedTotal())}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.blueAccent),
+              ),
+              onPressed: _selectedItems.isEmpty ? null : _navigateToCheckout,
+              child: Text(
+                'Mua hàng (${_selectedItems.length})',
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCartItem(CartItem item) {
+    return Dismissible(
+      key: Key(item.id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.red,
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) => _removeItem(item),
+      child: Card(
+        margin: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            const SizedBox(width: 8),
+            _buildCheckbox(item),
+            Expanded(
+              child: ListTile(
+                leading: _buildItemImage(item),
+                title: _buildItemTitle(item),
+                subtitle: _buildItemSubtitle(item),
+              ),
+            ),
+            _buildQuantityControls(item),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCheckbox(CartItem item) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 0),
+      child: Checkbox(
+        value: _selectedItems.contains(item.id),
+        onChanged: (bool? value) {
+          setState(() {
+            value ?? false
+                ? _selectedItems.add(item.id)
+                : _selectedItems.remove(item.id);
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildItemImage(CartItem item) {
+    return item.imageUrl != null
+        ? Container(
+          width: 70,
+          height: 70,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(item.imageUrl!, fit: BoxFit.cover),
+          ),
+        )
+        : const SizedBox(
+          width: 70,
+          height: 70,
+          child: Icon(Icons.image_not_supported),
+        );
+  }
+
+  Widget _buildItemTitle(CartItem item) {
+    return Text(
+      item.productName,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
+  }
+
+  Widget _buildItemSubtitle(CartItem item) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (item.discountRate != null && item.discountRate! > 0)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 4),
+              Text(
+                Utils.formatCurrency(item.price),
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                'Giảm giá: ${item.discountRate}%',
+                style: const TextStyle(color: Colors.red),
+              ),
+              const SizedBox(height: 4),
+
+              Text(
+                Utils.formatCurrency(item.priceAfterDiscount!),
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
+              ),
+              const SizedBox(height: 8),
+            ],
+          )
+        else
+          Text(
+            Utils.formatCurrency(item.price),
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent,
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _buildQuantityControls(CartItem item) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () => _updateItemQuantity(item, item.quantity - 1),
+        ),
+        Text('${item.quantity}'),
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () => _updateItemQuantity(item, item.quantity + 1),
+        ),
+      ],
+    );
+  }
+}
